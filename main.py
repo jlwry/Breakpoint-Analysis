e22b8d15c14123150078f54d2c0ec564da2e720a
jlwry <joshualowery101@gmail.com>
2025-10-23 17:42:45 -0400

    added comment to explain main.py file

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from utils.pipeline import breakpoint
 
+"""
+Example usage of the breakpoint analysis pipeline.
+
+This script demonstrates how to analyze moment-angle data from a participant.
+Modify the file path and column indices to analyze your own data.
+"""
+
 if __name__ == "__main__":
     breakpoint('data/example_data.txt', angle_column=1, moment_column=2, standing_column=5)
 
