b35aa6abf580ea8125ecf4008df6c9f9f2aa1851
jlwry <joshualowery101@gmail.com>
2025-10-23 14:34:39 -0400

    reads in the new data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils.pipeline import breakpoint
 
 if __name__ == "__main__":
-    breakpoint('data/A025_processed.txt', angle_column=1, moment_column=2, standing_column=5)
+    breakpoint('data/example_data.txt', angle_column=1, moment_column=2, standing_column=5)
 
 
 
