9d30dc9b0021326619e1b1025320f742251fa40d
jlwry <joshualowery101@gmail.com>
2025-10-24 08:16:06 -0400

    modified function structure

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ Modify the file path and column indices to analyze your own data.
 from utils.pipeline import breakpoint
 
 def main():
-    breakpoint('data/example_data.txt', angle_column=1, moment_column=2, standing_column=5)
+    breakpoint(
+        data = 'data/example_data.txt',
+        angle_column=1,
+        moment_column=2,
+        standing_column=5
+    )
 
 if __name__ == "__main__":
     main()
